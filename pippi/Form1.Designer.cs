ba11669650557a8f02fca6e7658ae31c3c3e155c
fivezett <zeki.length@gmail.com>
2023-01-15 19:01:56 +0900

    First Comit

---
+++ pippi/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace pippi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.windowComboBox = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // windowComboBox
+            // 
+            this.windowComboBox.FormattingEnabled = true;
+            this.windowComboBox.Location = new System.Drawing.Point(9, 12);
+            this.windowComboBox.Name = "windowComboBox";
+            this.windowComboBox.Size = new System.Drawing.Size(266, 20);
+            this.windowComboBox.TabIndex = 0;
+            this.windowComboBox.DropDown += new System.EventHandler(this.windowSelectorFocus);
+            this.windowComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(269, 23);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "PIP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 23);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "unPIP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 96);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.windowComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "pippi α";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox windowComboBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ pippi/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace pippi
+{
+    public partial class Form1 : Form
+    {
+
+        //==========================
+        //Win32API Import START
+        //==========================
+
+        //RECT 
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        private enum SWP : int
+        {
+            NOSIZE = 0x0001,
+            NOMOVE = 0x0002,
+            NOZORDER = 0x0004,
+            NOREDRAW = 0x0008,
+            NOACTIVATE = 0x0010,
+            FRAMECHANGED = 0x0020,
+            SHOWWINDOW = 0x0040,
+            HIDEWINDOW = 0x0080,
+            NOCOPYBITS = 0x0100,
+            NOOWNERZORDER = 0x0200,
+            NOSENDCHANGING = 0x400
+        }
+        private enum GWL : int
+        {
+            WINDPROC = -4,
+            HINSTANCE = -6,
+            HWNDPARENT = -8,
+            ID = -12,
+            STYLE = -16,
+            EXSTYLE = -20,
+            USERDATA = -21,
+        }
+
+        private const int SWP_NOSIZE = 0x0001;
+        private const int SWP_NOMOVE = 0x0002;
+        private const int SWP_SHOWWINDOW = 0x0040;
+
+        private const int HWND_TOPMOST = -1;
+        private const int HWND_NOTOPMOST = -2;
+
+        private const int SRCCOPY = 13369376;
+        private const int DWMWA_EXTENDED_FRAME_BOUNDS = 9;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int EnumWindows(EnumWindowsDelegate lpEnumFunc, IntPtr lparam);
+        public delegate bool EnumWindowsDelegate(IntPtr hWnd, IntPtr lparam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int index);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int index, int unValue);
+
+        [DllImport("user32")]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        Dictionary<int, IntPtr> windowList = new Dictionary<int, IntPtr>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            IntPtr target = windowList[windowComboBox.SelectedIndex];
+        }
+
+        private void windowSelectorFocus(object sender, EventArgs e)
+        {
+            windowComboBox.Items.Clear();
+            windowList.Clear();
+            EnumWindows(new EnumWindowsDelegate(EnumWindowCallBack), IntPtr.Zero);
+        }
+
+        private bool EnumWindowCallBack(IntPtr hWnd, IntPtr lparam)
+        {
+            //https://www.natsuneko.blog/entry/2018/08/09/002742
+            //不可視化ウィンドウ除外
+            if (!IsWindowVisible(hWnd))
+                return true;
+
+            // タイトル空
+            var title = new StringBuilder(1024);
+            GetWindowText(hWnd, title, title.Capacity);
+            if (string.IsNullOrWhiteSpace(title.ToString()))
+                return true; // Skipped
+
+            int textLen = GetWindowTextLength(hWnd);
+            if (0 < textLen)
+            {
+                StringBuilder tsb = new StringBuilder(textLen + 1);
+                GetWindowText(hWnd, tsb, tsb.Capacity);
+
+                windowComboBox.Items.Add(tsb.ToString());
+                windowList.Add(windowList.Count, hWnd);
+            }
+
+            //すべてのウィンドウを列挙する
+            return true;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (windowList.Count == 0) return;
+            IntPtr hWnd;
+            try
+            {
+                hWnd = windowList[windowComboBox.SelectedIndex];
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            SetWindowLong(hWnd, -16, GetWindowLong(hWnd, -16) & ~(0x00800000 | 0x00400000));
+            SetWindowPos(hWnd, -1, 0, 0, 0, 0, 0x0020| 0x0002 | 0x0001);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (windowList.Count == 0) return;
+            IntPtr hWnd;
+            try
+            {
+                hWnd = windowList[windowComboBox.SelectedIndex];
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            SetWindowLong(hWnd, -16,0x14CF0000);
+            SetWindowPos(hWnd,-2, 0, 0, 0, 0, 0x0020 | 0x0002 | 0x0001);
+        }
+    }
+}
